0da311d4d08a697ff4d2e746f6732f8baf10daa2
CarolinaK03 <122482180+CarolinaK03@users.noreply.github.com>
2026-02-09 12:56:51 -0500

    setting up

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class RunProject1{
-    public static voidjnnnkjn
+    public static void main(String[] args) {
+        new Frontend().run();
+    }
 }
