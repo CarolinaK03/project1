60a3e333f99ed43c0ac8364c2b3ef20fe0e458fc
CarolinaK03 <122482180+CarolinaK03@users.noreply.github.com>
2026-02-11 00:36:28 -0500

    Date class work

--- src/Date.java
+++ src/Date.java
@@ -3,14 +3,76 @@ public class Date implements Comparable<Date>{
     private int month;
     private int day;
 
-    public boolean isValid(){
+    public static final int QUADRENNIAL = 4;
+    public static final int CENTENNIAL = 100;
+    public static final int QUARTERCENTENNIAL = 400;
+    //public static final int JAN = Calendar.JANUARY + 1;//Calendar.JANUARY is 0
+    public static final int MAXDAYSINAMONTH = 31;
+    public static final int MONTHS = 12;
+    public static final int DAYSINAMONTH = 30;
+    public static final int DAYSINLEAPFEB = 28;
+    public static final int FEB = 2;
+    public static final int[] MONTHS30DAYS = {2,4,5,9,11};
+    public Date(int month, int day, int year){
+        this.month = month;
+        this.day = day;
+        this.year = year;
+    }
 
+    public boolean isValid() {
+        //08/03/2005
+        // checking year
+        if (year > 2026 || year <= 0) {
+            return false;
+        }
+        // checking month
+        if (month > 12 || month <= 0) {
+            return false;
+        }
+        // checking day
+        if (day < 1 || day > 31) {
+            return false;
+        }
+        // checking feb
+        if (month == 2){
+            if (isLeap() && day > 28){
+                return false;
+            }
+        }
+
+
+
+
+
+
+        if (year <= 2026 && year > 0) {
+            // move onto validating months
+            if (month <= MONTHS && month >= 1) {
+                // move onto validating days
+                if (day <= 31 && day >= 1) {
+                    // move onto checking for leap year
+                    if (isLeap(year) && month == FEB && day > DAYSINLEAPFEB) {
+                        return false;
+                    }
+                    for (int n:MONTHS30DAYS) {
+                        if (n == month){
+                            if (day > 30) {
+                                return false;
+                            } else {
+                                return true;
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
     }
 
     public boolean isLeap(int year){
-        if (year % 4 == 0){
-            if(year % 100 == 0){
-                if(year % 400 == 0){
+        if (year % QUADRENNIAL == 0){
+            if(year % CENTENNIAL == 0){
+                if(year % QUARTERCENTENNIAL == 0){
                     return true;
                 } else { return false; }
             } else { return true; }
--- src/Frontend.java
+++ src/Frontend.java
@@ -37,6 +37,38 @@ public class Frontend {
 
     public void addStudent(){
         system.out.println("Please enter the first name, last name, date of birth mm/dd/yyyy, major, and number of completed credits.");
+        // Read all data tokens and store them temporarily.
+        // If token is not accurate, remove it from that variable and ask again
+        // once all are checked and validated, constuct a student profile
+        // construct a student
+        // add student to student list
+
+        int i = 0;
+            while (i< args.length) {
+                string tempFName = arg[0];
+                string tempLName = arg[1];
+
+                // creating temporary date to insert later
+                string tempDateStr = arg[2];
+                string[] dateParts = tempdateStr.split("/");
+                int month = Integer.parseInt(dateParts[0]);
+                int day = Integer.parseInt(dateParts[1]);
+                int year = Integer.parseInt(dateParts[2]);
+                // validating date
+                Date tempDate = new Date(month,day,year);
+                while(!tempdate.isValid()){
+
+                }
+                // removing or finalizing date
+
+                string major = arg[3];
+                int tempCredits = arg[4]
+
+
+
+
+            }
+
 
     }
 }
